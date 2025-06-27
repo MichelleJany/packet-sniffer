313d0c8d4604bc30389a19f0a1b5b9567fe56145
Michelle Jany <138966680+MichelleJany@users.noreply.github.com>
2025-06-27 21:56:35 +0200

    - Fixed filters, - Added user-defined count

--- packet-sniffer.py
+++ packet-sniffer.py
@@ -3,11 +3,11 @@ from scapy.all import sniff
 def packet_callback(packet):
     print(packet.summary())
 
-def sniffing(packet):
+def start_sniffing(filter, count):
     sniff(
+        filter=filter,
         prn=packet_callback,
-        timeout=10,
-        count=1
+        count=count,
         )
 
 def sniff_menu():
@@ -19,26 +19,31 @@ def sniff_menu():
         print("3. Quit")
         choice = input("Enter your choice: ")
 
+        count = int(input("How many packets do you want to capture? "))
+
         if choice == '1':
-            sniffing()
+            start_sniffing(filter=None, count=count)
 
         elif choice == '2':
             print("1. ICMP (ping)")
             print("2. UDP")
             print("3. HTTP (TCP port 80)")
             print("4. DNS (UDP port 53)")
-            print("5. Quit")
+            print("5. TCP")
+            print("6. Quit")
 
             filter_choice = input("Enter your choice: ")
             if filter_choice == '1':
-                sniff(filter='icmp', prn=sniffing)
+                start_sniffing(filter='icmp', count=count)
             elif filter_choice == '2':
-                sniff(filter='udp', prn=sniffing)
+                start_sniffing(filter='udp', count=count)
             elif filter_choice == '3':
-                sniff(filter='tcp port 80', prn=sniffing)
+                start_sniffing(filter='tcp port 80', count=count)
             elif filter_choice == '4':
-                sniff(filter='udp port 53', prn=sniffing)
+                start_sniffing(filter='udp port 53', count=count)
             elif filter_choice == '5':
+                start_sniffing(filter='tcp', count=count)
+            elif filter_choice == '6':
                 print("Goodbye!")
                 break
             else:
@@ -54,6 +59,3 @@ def sniff_menu():
             break
         
 sniff_menu()
-
-
-
